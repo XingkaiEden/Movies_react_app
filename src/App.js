0a485ef80fd5ac21411f8e83174f1b518256a1c0
Xingkai Yang <xingkaiyang@Xingkais-MacBook-Pro.local>
2019-10-17 17:00:33 -0400

    navBar has been added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NavBar from './components/common/navBar';
+import NotFound from "./components/common/notFound";
+
 import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
+      <main className="container-fluid">
+        <NavBar />
+        <Switch>
+
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" to="/movies" />
+
+          <Redirect to="/not-found" />
+        </Switch>
+
+        {/* 
+        <Movies /> */}
       </main>
     );
   }
